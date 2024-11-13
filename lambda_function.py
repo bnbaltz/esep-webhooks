d42ed77f2037ac11d6376787b5e99a991bf0bf04
bnbaltz <56062497+bnbaltz@users.noreply.github.com>
2024-11-12 19:42:02 -0800

    change from requets

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,10 @@
 import os
-import requests
-import logging
+import urllib3
 
+http = urllib3.PoolManager()
+
+import logging
+import json
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -9,15 +12,16 @@ def lambda_handler(event, context):
     # Log the input received
     logger.info(f"FunctionHandler received: {event}")
 
-    payload = {
+    payload = json.dumps({
         "text": f"Issue Created: {event['issue']['html_url']}"
-    }
+    })
 
     slack_url = os.getenv("SLACK_URL")
-    response = requests.post(slack_url, json=payload, headers={"Content-Type": "application/json"})
-
-    if response.ok:
-        return response.text
+    r = http.request('POST', slack_url,
+                 headers={'Content-Type': 'application/json'},
+                 body=payload)
+    if r:
+        return r.read()
     else:
         logger.error(f"Request to Slack failed: {response.status_code} - {response.text}")
         return f"Error: {response.status_code}"
